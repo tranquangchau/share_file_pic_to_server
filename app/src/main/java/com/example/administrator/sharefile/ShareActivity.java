a2a82faf0d737860d9e37a68366393c0471d06f5
chau tran <chau tran>
2016-06-18 10:13:15 +0700

    google plus

--- app/src/main/java/com/example/administrator/sharefile/ShareActivity.java
+++ app/src/main/java/com/example/administrator/sharefile/ShareActivity.java
@@ -6,7 +6,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
 import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -17,22 +20,30 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Random;
 
 public class ShareActivity extends AppCompatActivity {
 
     TextView messageText;
     ProgressDialog dialog = null;
     int serverResponseCode = 0;
+    static String sal= "ShareActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+        Log.d(sal, "onCreate");
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
@@ -49,54 +60,217 @@ public class ShareActivity extends AppCompatActivity {
         Log.d("Resource", getResources().getText(R.string.send_to).toString());*/
 
 
-        //final Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        //shareIntent.setType("image/jpg");
-        //final File photoFile = new File(getFilesDir(), "foo.jpg");
-        Intent receivedIntent = getIntent();
-        String receivedAction = receivedIntent.getAction();
-        String receivedType = receivedIntent.getType();
-        //String name = intent.getData().getLastPathSegment();
-        if(receivedType.startsWith("image/")){
-            //handle sent image
-        }
-        Uri receivedUri = (Uri)receivedIntent.getParcelableExtra(Intent.EXTRA_STREAM);
-        String Url_file=receivedUri.toString();
-        if(!receivedUri.toString().contains("storage/sdcard0")){
-            //http://stackoverflow.com/a/20059657
-             Url_file = getRealPathFromUri(ShareActivity.this,receivedUri);
-        }
-        if (Url_file != null) {
-            //set the picture
-            //RESAMPLE YOUR IMAGE DATA BEFORE DISPLAYING
-            //picView.setImageURI(Url_file);//just for demonstration
-            Log.d("File_non", Url_file.toString());
-            //final String uploadFilePath = "/mnt/sdcard/";
-            //string.replace("=\"ppshein\"", "");
-
-            String m = Url_file.toString();
-            m=m.replace("file:///storage/sdcard0/","/mnt/sdcard/");
-            //m=m.replace(" ","\\ ");
-            m=m.replace("%20"," ");
-            Log.d("File_non", m);
-            //shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(photoFile));
-            //startActivity(Intent.createChooser(shareIntent, "Share image using"));
-            //get data save url
-            SharedPreferences sp = getSharedPreferences("your_prefs", Activity.MODE_PRIVATE);
-            String myStrValue = sp.getString("url_server_link", "null");
-            Log.d("myStrValue_sharefile", myStrValue);
-
-            upLoadServerUri=myStrValue;
-            if(myStrValue=="null"){
-                upLoadServerUri=getResources().getText(R.string.url_link_default).toString();
+        try {
+            Log.d(sal, "Start try");
+            //final Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            //shareIntent.setType("image/jpg");
+            //final File photoFile = new File(getFilesDir(), "foo.jpg");
+            Intent receivedIntent = getIntent();
+            String receivedAction = receivedIntent.getAction();
+            String receivedType = receivedIntent.getType();
+            Log.d(sal, "receivedType "+receivedType);
+            //String name = intent.getData().getLastPathSegment();
+            if(receivedType.startsWith("image/")){
+                //handle sent image
             }
-            messageText.setText("Server="+upLoadServerUri);
+            Uri receivedUri = (Uri)receivedIntent.getParcelableExtra(Intent.EXTRA_STREAM);
+            String Url_file=receivedUri.toString();
+            Log.d(sal, "Url_file "+ Url_file.toString());
+            Log.d(sal, "receivedUri1 "+ receivedUri.toString());
+            if(receivedUri.toString().contains("content://com.mobisystems")){
+                //http://stackoverflow.com/a/20059657
+                Log.d(sal, "receivedUri2  "+ "content://com.mobisystems");
+                Url_file = getRealPathFromUri(ShareActivity.this,receivedUri);
+            }else if(receivedUri.toString().contains("content://com.google.android.apps.photos.content")){
+                Log.d(sal, "trung_voi  "+ "content://com.google.android.apps.photos.content");
+                Uri url1= Uri.parse(receivedUri.toString());
+                Log.d(sal, "url1  " + url1);
+                String mimeType = getContentResolver().getType(url1);
+                Log.d(sal, "mimeType  "+ mimeType);
+                //String m = getImageUrlWithAuthority(ShareActivity.this, url1);
+                //Log.d(sal, "mfile  "+ m); //kiem hinh anh
 
-            uploadFile(m);
-            //share_file(m);
+                Bitmap bitmap = null;
+                InputStream is = null;
+                is = getContentResolver().openInputStream(url1);
+                bitmap = BitmapFactory.decodeStream(is);
+
+                String root = Environment.getExternalStorageDirectory().toString();
+                File myDir = new File(root + "/saved_images");
+                myDir.mkdirs();
+                Random generator = new Random();
+                int n = 10000;
+                n = generator.nextInt(n);
+                String fname = "Image-"+ n +".jpg";
+                File file = new File (myDir, fname);
+                if (file.exists ()) file.delete ();
+                try {
+                    //finalBitmap = rotate(bmp,50);
+                    FileOutputStream out = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+                    out.flush();
+                    out.close();
+                    Log.d(sal, "file  " + file); //hinh anh
+                    Url_file= String.valueOf(file);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                //String m2 =getRealPathFromURI(ShareActivity.this,Uri.parse(m));
+                //Url_file= m2;
+
+                //download
+//                downloadFile(Url_file,"sdcard");
+
+                //upload file
+            }else{
+                //file:///storage/sdcard0/Images_SellChat/photo-2369.jpg
+                Log.d(sal, "receivedUri  "+ "else");
+            }
+            if (Url_file != null) {
+                //set the picture
+                //RESAMPLE YOUR IMAGE DATA BEFORE DISPLAYING
+                //picView.setImageURI(Url_file);//just for demonstration
+                Log.d(sal,"File_non"+ Url_file.toString());
+                //final String uploadFilePath = "/mnt/sdcard/";
+                //string.replace("=\"ppshein\"", "");
+
+                String m = Url_file.toString();
+                m=m.replace("file:///storage/sdcard0/","/mnt/sdcard/");
+                //m=m.replace(" ","\\ ");
+                m=m.replace("%20"," ");
+                if(m.contains("content://media/external/images")){
+                    m = getRealPathFromUri(ShareActivity.this, Uri.parse(m));
+                }
+                Log.d(sal,"File_non1"+ m);
+                //shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(photoFile));
+                //startActivity(Intent.createChooser(shareIntent, "Share image using"));
+                //get data save url
+                SharedPreferences sp = getSharedPreferences("your_prefs", Activity.MODE_PRIVATE);
+                String myStrValue = sp.getString("url_server_link", "null");
+                Log.d(sal,"myStrValue_sharefile"+ myStrValue);
+
+                upLoadServerUri=myStrValue;
+                if(myStrValue=="null"){
+                    upLoadServerUri=getResources().getText(R.string.url_link_default).toString();
+                }
+                messageText.setText("Server="+upLoadServerUri);
+
+                uploadFile(m);
+                //share_file(m);
+            }
+        }catch (Exception e){
+            Log.d(sal,"Exception 1 Error here");
+           e.printStackTrace();
+        }
+    }
+
+
+
+    /**
+     * http://stackoverflow.com/a/31019925
+     * @param context
+     * @param uri
+     * @return
+     */
+    public static String getImageUrlWithAuthority(Context context, Uri uri) {
+        InputStream is = null;
+        if (uri.getAuthority() != null) {
+            try {
+                is = context.getContentResolver().openInputStream(uri);
+                Bitmap bmp = BitmapFactory.decodeStream(is);
+                //is.reset();
+                return writeToTempImageAndGetPathUri(context, bmp).toString();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    public static Uri writeToTempImageAndGetPathUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        //bytes.reset();
+        String path="";
+        Log.d(sal,inImage.toString());
+        if(inImage.toString().equals("")){
+            Log.d(sal,"inImage Null");
+        }
+        if(inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)){
+            path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        }if(inImage.compress(Bitmap.CompressFormat.PNG, 100, bytes)){
+            path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        }if(inImage.compress(Bitmap.CompressFormat.valueOf("GIF"), 100, bytes)){
+            path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
         }
 
-
+        //String path = MediaStore.Images.Media.insertImage("sdcard", inImage, "Title", null);
+        return Uri.parse(path);
     }
+    public static void downloadFile(String fileURL, String saveDir)
+            throws IOException {
+        URL url = new URL(fileURL);
+        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+        int responseCode = httpConn.getResponseCode();
+
+        // always check HTTP response code first
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            String fileName = "";
+            String disposition = httpConn.getHeaderField("Content-Disposition");
+            String contentType = httpConn.getContentType();
+            int contentLength = httpConn.getContentLength();
+
+            if (disposition != null) {
+                // extracts file name from header field
+                int index = disposition.indexOf("filename=");
+                if (index > 0) {
+                    fileName = disposition.substring(index + 10,
+                            disposition.length() - 1);
+                }
+            } else {
+                // extracts file name from URL
+                fileName = fileURL.substring(fileURL.lastIndexOf("/") + 1,
+                        fileURL.length());
+            }
+
+            System.out.println("Content-Type = " + contentType);
+            System.out.println("Content-Disposition = " + disposition);
+            System.out.println("Content-Length = " + contentLength);
+            System.out.println("fileName = " + fileName);
+
+            // opens input stream from the HTTP connection
+            InputStream inputStream = httpConn.getInputStream();
+            String saveFilePath = saveDir + File.separator + fileName;
+
+            // opens an output stream to save into file
+            FileOutputStream outputStream = new FileOutputStream(saveFilePath);
+
+            int bytesRead = -1;
+            byte[] buffer = new byte[1200];
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+
+            outputStream.close();
+            inputStream.close();
+
+            System.out.println("File downloaded");
+        } else {
+            System.out.println("No file to download. Server replied HTTP code: " + responseCode);
+        }
+        httpConn.disconnect();
+    }
+    //http://stackoverflow.com/a/3414749
+    //how to convert `content://media/external/images/media/Y` to `file:///storage/sdcard0/Pictures/X.jpg`
     public static String getRealPathFromUri(Context context, Uri contentUri) {
         Cursor cursor = null;
         try {
@@ -139,7 +313,7 @@ public class ShareActivity extends AppCompatActivity {
 
             dialog.dismiss();
 
-            Log.e("uploadFile", "Source File not exist :"
+            Log.e(sal,"uploadFile Source File not exist :"
                     + uploadFilePath + "" + uploadFileName);
 
             runOnUiThread(new Runnable() {
@@ -172,7 +346,7 @@ public class ShareActivity extends AppCompatActivity {
                 conn.setRequestProperty("ENCTYPE", "multipart/form-data");
                 conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
                 String m=fileName.replace(" ","");
-                Log.d("File_name", m);
+                Log.d(sal,"File_name"+ m);
                 conn.setRequestProperty("uploaded_file", fileName);
 
                 dos = new DataOutputStream(conn.getOutputStream());
@@ -182,6 +356,7 @@ public class ShareActivity extends AppCompatActivity {
                         + m + "" + lineEnd);
 
                 dos.writeBytes(lineEnd);
+
 
                 // create a buffer of  maximum size
                 bytesAvailable = fileInputStream.available();
@@ -205,11 +380,13 @@ public class ShareActivity extends AppCompatActivity {
                 dos.writeBytes(lineEnd);
                 dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
 
+                Log.d(sal,"dos "+ dos.toString());
                 // Responses from the server (code and message)
                 serverResponseCode = conn.getResponseCode();
                 String serverResponseMessage = conn.getResponseMessage();
-
-                Log.i("uploadFile", "HTTP Response is : "
+                //String s_reson=  conn.getContentEncoding().toString();
+               // Log.d(sal,"s_reson "+s_reson);
+                Log.i(sal,"uploadFile"+ "HTTP Response is : "
                         + serverResponseMessage + ": " + serverResponseCode);
 
                 if(serverResponseCode == 200){
@@ -229,6 +406,7 @@ public class ShareActivity extends AppCompatActivity {
                 }
 
                 //close the streams //
+                //Log.d(sal,"fileInputStream "+fileInputStream.read());
                 fileInputStream.close();
                 dos.flush();
                 dos.close();
@@ -236,6 +414,7 @@ public class ShareActivity extends AppCompatActivity {
             } catch (MalformedURLException ex) {
 
                 dialog.dismiss();
+                Log.d(sal,"Exception 2"+"Error here");
                 ex.printStackTrace();
 
                 runOnUiThread(new Runnable() {
@@ -246,10 +425,11 @@ public class ShareActivity extends AppCompatActivity {
                     }
                 });
 
-                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+                Log.e(sal,"Upload file to server"+ "error: " + ex.getMessage(), ex);
             } catch (Exception e) {
 
                 dialog.dismiss();
+                Log.d(sal,"Exception 3"+ "Error here");
                 e.printStackTrace();
 
                 runOnUiThread(new Runnable() {
@@ -259,7 +439,7 @@ public class ShareActivity extends AppCompatActivity {
                         //        Toast.LENGTH_SHORT).show();
                     }
                 });
-                Log.e("Upload_Excepti", "Exception : "
+                Log.e(sal,"Upload_Excepti"+ "Exception : "
                         + e.getMessage(), e);
             }
             dialog.dismiss();
